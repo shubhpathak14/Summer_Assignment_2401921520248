b277af986b4d4ecc04f0e6c49336dff46ff25186
Shubh Pathak <shubhpathak312@gmail.com>
2026-06-04 21:46:42 +0530

    Add Matrix Diagonal Sum solution

---
+++ Week_1/Day_4/Matrix_Diagonal_Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i];             // Primary diagonal
+            sum += mat[i][n - 1 - i];     // Secondary diagonal
+        }
+
+        // Remove duplicate center element for odd-sized matrix
+        if (n % 2 == 1) {
+            sum -= mat[n / 2][n / 2];
+        }
+
+        return sum;
+    }
+};
