141cc04591acaff4cc8f40c147dad6bcb287596b
Shubh Pathak <shubhpathak312@gmail.com>
2026-06-09 21:28:40 +0530

    Add Valid Anagram solution

---
+++ Week_2/Day_1/Valid_Anagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        vector<int> count(26, 0);
+
+        for (char ch : s) {
+            count[ch - 'a']++;
+        }
+
+        for (char ch : t) {
+            count[ch - 'a']--;
+        }
+
+        for (int freq : count) {
+            if (freq != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
