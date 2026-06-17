bbd695c0999eed9ce0dd848aabf76f41d0ff4fe0
Shubh Pathak <shubhpathak312@gmail.com>
2026-06-17 20:47:52 +0530

    Add Palindrome Linked List solution

---
+++ Week_3/Day_1/Week_3/Day_2/Palindrome_Linked_List.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+
+        if (head == NULL || head->next == NULL)
+            return true;
+
+        // Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse second half
+        ListNode* prev = NULL;
+        while (slow) {
+            ListNode* nextNode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        // Compare both halves
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while (second) {
+            if (first->val != second->val)
+                return false;
+
+            first = first->next;
+            second = second->next;
+        }
+
+        return true;
+    }
+};
