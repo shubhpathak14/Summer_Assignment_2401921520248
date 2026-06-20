8f9940a65712f3cfea1ceabdbcf3f73cb30485ed
Shubh Pathak <shubhpathak312@gmail.com>
2026-06-20 21:49:09 +0530

    Add Sliding Window Maximum solution

---
+++ Week_3/Day_1/Week_3/Day_5/Sliding_Window_Maximum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+
+        deque<int> dq;
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            // Remove indices outside the window
+            while (!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+
+            // Remove smaller elements
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+
+            // Window of size k formed
+            if (i >= k - 1) {
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+
+        return ans;
+    }
+};
